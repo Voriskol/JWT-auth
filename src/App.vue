dac93202c0360dbf5b85d8e3af5293f668febe20
Kirill <pesterevkv2003@gmail.com>
2024-08-10 15:04:27 +0300

    upd: added dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
 <template>
-  <h1>Hello</h1>
+  <h1 class="text-3xl font-bold underline">Hello</h1>
+  <Button label="Button" />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { initializeApp } from 'firebase/app'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 import App from '@/App.vue'
 // import router from './router'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAlEc_0NMnGcgBEiv3eaFxwzM0jyAIaruQ',
+  authDomain: 'jwt-auth-f4624.firebaseapp.com',
+  projectId: 'jwt-auth-f4624',
+  storageBucket: 'jwt-auth-f4624.appspot.com',
+  messagingSenderId: '732039012335',
+  appId: '1:732039012335:web:f2f560becb00402f17e01a'
+}
+
+initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
 app.use(createPinia())
 // app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
 
 app.mount('#app')

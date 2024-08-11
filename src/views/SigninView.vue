d62872acfe66172c01453ac814ad7a02b1b579bf
Kirill <pesterevkv2003@gmail.com>
2024-08-11 16:15:06 +0300

    upd: added styles

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -20,25 +20,25 @@ const signin = async () => {
 </script>
 
 <template>
-  <h2 class="text-3xl mx-auto text-center mt-10 mb-3">Sign In</h2>
-  <form class="flex flex-col gap-3 items-center">
-    <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
-    <div class="p-inputgroup flex-1">
-      <span class="p-inputgroup-addon pr-3">
+  <h2 class="text-3xl mx-auto text-center mt-10 mb-3">Sign Up</h2>
+  <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
+  <form class="flex flex-col gap-3 border-2 border-green-400 rounded p-3">
+    <div class="flex p-inputgroup flex-1">
+      <span class="p-inputgroup-addon pr-3 pt-2">
         <i class="pi pi-user"></i>
       </span>
-      <InputText type="email" v-model="email" placeholder="Your Email" />
+      <InputText class="w-full" type="email" v-model="email" placeholder="Your Email" />
     </div>
-    <div class="p-inputgroup flex-1">
-      <span class="p-inputgroup-addon pr-3">
+    <div class="flex p-inputgroup flex-1">
+      <span class="p-inputgroup-addon pr-3 pt-2">
         <i class="pi pi-at"></i>
       </span>
-      <InputText type="password" v-model="password" placeholder="Password" />
+      <InputText class="w-full" type="password" v-model="password" placeholder="Password" />
     </div>
     <Loader v-if="authStore.loader" />
     <div v-else class="flex flex-col gap-3">
       <Button label="Sign in" @click="signin" />
-      <span
+      <span class="text-center"
         >Are you not registered yet?
         <router-link @click="authStore.error = ''" to="/signup">Sign up</router-link></span
       >
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -21,24 +21,24 @@ const signup = async () => {
 
 <template>
   <h2 class="text-3xl mx-auto text-center mt-10 mb-3">Sign Up</h2>
-  <form class="flex flex-col gap-3 items-center">
-    <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
-    <div class="p-inputgroup flex-1">
-      <span class="p-inputgroup-addon pr-3">
+  <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
+  <form class="flex flex-col gap-3 border-2 border-green-400 rounded p-3">
+    <div class="flex p-inputgroup flex-1">
+      <span class="p-inputgroup-addon pr-3 pt-2">
         <i class="pi pi-user"></i>
       </span>
-      <InputText type="email" v-model="email" placeholder="Your Email" />
+      <InputText class="w-full" type="email" v-model="email" placeholder="Your Email" />
     </div>
-    <div class="p-inputgroup flex-1">
-      <span class="p-inputgroup-addon pr-3">
+    <div class="flex p-inputgroup flex-1">
+      <span class="p-inputgroup-addon pr-3 pt-2">
         <i class="pi pi-at"></i>
       </span>
-      <InputText type="password" v-model="password" placeholder="Password" />
+      <InputText class="w-full" type="password" v-model="password" placeholder="Password" />
     </div>
     <Loader v-if="authStore.loader" />
     <div v-else class="flex flex-col gap-3">
       <Button label="Sign up" @click="signup" />
-      <span class=""
+      <span class="text-center"
         >Are you already registered?
         <router-link @click="authStore.error = ''" to="/signin">Sign in</router-link></span
       >
